f376f14e8febb9818147dab2c286d6d52ca2b577
lbbgreat2 <875681962@qq.com>
2026-04-10 15:30:08 +0800

    server.js

--- server.js
+++ server.js
@@ -44,50 +44,60 @@ const ENJOY_AI_HIERARCHICAL_LINKS = {
     }
   },
   '2026': {
-    // 根据文档内容，2026年更新为以下四个新项目
-    'inventions_trail': {
-      'competition_rule': 'https://www.dropbox.com/scl/fi/5qprcf8ezy26gqpey5tnp/compeition-rule.pdf?rlkey=ktvu3885ph0vw8yr6b2d1pr2a&st=qmiin83o&dl=0',
-      'parts_list': 'https://www.dropbox.com/scl/fi/4tb83c3w9zzjfhfxz56ux/EA-P1-26-EN-Parts-List-260212.pdf?rlkey=gcj1tcf0q0lku4tigv2gka13e&st=mmaq57g7&dl=0',
-      'competition_rules_video': 'https://www.dropbox.com/scl/fi/rjhc2q9cddevj2h8dwfqr/ENJOY-AI-2026-Inventions-Trail-Competition-Rules.mp4?rlkey=2sr8ixwpx93g6awtcc4tklv0r&st=b0ws6wk4&dl=0',
-      'full_competition_video': 'https://www.dropbox.com/scl/fi/318zktxprqe8dv9cpk6tr/Enjoy-AI-2026-Inventions-Trail-full-competition-round.mp4?rlkey=sw8ccmwoylc28s96te6luvenf&st=n4t77o73&dl=0',
-      'field_setup_guide': 'https://www.dropbox.com/scl/fo/uv1aqaoopgly0tyuyit4i/AFuwkxQHWJ9vW22o75xCdJE?rlkey=9p52ziaha7snk8lmthpwtdh9u&st=kvxtyiic&dl=0',
-      'product_list': 'https://www.dropbox.com/scl/fo/7oj3k7pc0tk4whhw7twjx/AKvxOsp6Is0_lYh0orNvrms?rlkey=8tjej12f7vkka9zjmfhvcy6zo&st=wm1m4bi0&dl=0',
-      'program_reference': 'https://www.dropbox.com/scl/fo/lqj1yp7e15w760sgg4oo1/AJQmPDtNZSocYvMg88rlAhs?rlkey=0009ly68xuzkp5dvcjl6s1ipa&st=1w7bjhor&dl=0',
-      'solution_manual': 'https://www.dropbox.com/scl/fi/k4e3nc9nb738e1wzl054p/Enjoy-AI-2026-Inventions-Trail-Competition-Solution-Demonstration-Manual.pdf?rlkey=ehr2zgwafwwjpxrevqnsgtu8y&st=5tckg3ay&dl=0',
-      'air_pump_manual': 'https://www.dropbox.com/scl/fi/r8irw71cbf3bz2z2mdkgg/Air-pump-user-manual.pdf?rlkey=e0koldinv5qozowf7gtca88by&st=96p9s3ze&dl=0'
-    },
-    'battle_of_stars': {
-      'competition_rules': 'https://www.dropbox.com/scl/fo/p8wld9w2b7bzy7znpmz3q/AMxFA6RSBnu6HF8K2kz62eM?rlkey=zmzvdoku6q1uxwpk90mtv44g6&st=o3zp4629&dl=0',
-      'field_setup_guide': 'https://www.dropbox.com/scl/fi/pixz66quabudq32b56krw/ENJOY-AI-2026-Battle-of-stars-field-setup-guide.pdf?rlkey=qc5hnk5objvm0h5b0ibah2qk1&st=qolnnqdp&dl=0',
-      'parts_list': 'https://www.dropbox.com/scl/fi/f8u1nm8d7yus903srm8v2/EA-P2-26-EN-Parts-List.pdf?rlkey=uhrtvyp8vqdmj9vvh5bhm85x6&st=dpjgg35w&dl=0',
-      'competition_rules_video': 'https://www.dropbox.com/scl/fi/mauxwany05050stfngdl0/2026-Battle-of-Stars-Competition-Rules.mp4?rlkey=ljg7amefvy8dgqe19nl66p73x&st=0lmk4erc&dl=0',
-      'full_competition_video': 'https://www.dropbox.com/scl/fi/2qr9nquqz1byy2ikjhcw1/2026-Battle-of-Stars-Full-Competition-Round.mp4?rlkey=0ofvjxgjb6rit7vb48nscvj0x&st=0ubxut8k&dl=0',
-      'controller_manual': 'https://www.dropbox.com/scl/fi/d9x3wpqd6l65psiv9ohu0/MC102-User-Guide-V1.1_241014.pdf?rlkey=qseqp57zl99iaq4510paaqxes&st=3n1nm6u5&dl=0',
-      'product_list': 'https://www.dropbox.com/scl/fi/y0mabux20g4dxmcsinfwu/AI-Module-3s-Quick-Start-Product-List.pdf?rlkey=2voj08i6skowk4o3nrcz5s0ax&st=5ag84g0c&dl=0',
-      'program_reference': 'https://www.dropbox.com/scl/fo/70u4d6crex7mmkhp7zxms/ALj9a5V5xnF8RN2JkLHXGj4?rlkey=c8m8mi36wlw3jj8tiyz2nb9le&st=mdaj8uvv&dl=0',
-      'solution_manual': 'https://www.dropbox.com/scl/fi/pkuncpsh5jzittha4u1ms/Battle-of-stars-Solution-Construction-Manual.pdf?rlkey=wb8pbqdbzn9ba6f6gtaui3cus&st=p33nx0kf&dl=0'
-    },
-    'skyline_adventures': {
-      'competition_rule': 'https://www.dropbox.com/scl/fo/gl3pgideoqq84z6qw1fjs/AFjsE7V6zI7bYXzPusZbpa4?rlkey=auihwdlhy1z5wulhn4851w6et&st=ph4xrig8&dl=0',
-      'field_setup_guide': 'https://www.dropbox.com/scl/fi/as7b7huhcqicgio4anjwq/ENJOY-AI-2026-Skyline-Adventures-Competition-Field-up-Guide.pdf?rlkey=epdkryoxv4orf2pccdjn83ny9&st=0wgovay3&dl=0',
-      'parts_list': 'https://www.dropbox.com/scl/fo/vhuq1z0hfgi72nl7wmro5/AMsZGJ6QhHCDW-K_Rm6GlL8?rlkey=m8jy5d4gebr7soii8vyti70j6&st=eygt0mpa&dl=0',
-      'competition_rules_video': 'https://www.dropbox.com/scl/fi/c99jphn5fcba0pf5thaed/ENJOY-AI-2026-Skyline-Adventures-Competition-Rules.mp4?rlkey=33sk9ljligp4mosig6ercvumc&st=crqfjm7z&dl=0',
-      'full_competition_video': 'https://www.dropbox.com/scl/fi/z3wrh67ikct9j7u872rla/2026-Skyline-Adventure-Full-Competition-Round.mp4?rlkey=sgwghmpnb1xzunycu49140ud4&st=udn2pgvb&dl=0',
-      'controller_manual': 'https://www.dropbox.com/scl/fi/0pyomgux7z3ztujpzj5d6/WhalesBot-Eagle_User-Guide_V1.0.pdf?rlkey=rcl7bq0nk9wawm28mt1sgc1k4&st=0laml4cd&dl=0',
-      'product_list': 'https://www.dropbox.com/scl/fi/85yhg5c0msq2ukij9fjkk/Eagle-1003-Quick-Start-Product-List.pdf?rlkey=pa3zyk23phgw8993892m67kvn&st=t76v3q5u&dl=0',
-      'program_reference': 'https://www.dropbox.com/scl/fo/q6eu9ww622umlxxjoj5e3/AJeabNJgkxllD8T2OGSbvYw?rlkey=o7ve809qywc2otl8pw65zd84a&st=uv4uc0kg&dl=0'
-    },
-    'ancient_civilizations': {
-      'competition_rules': 'https://www.dropbox.com/scl/fo/abnf2uufaifqhbr0mmplb/AHITzxaVbJ9qhSVuhl1mbwY?rlkey=bt61eqfrwsvofpkzacf2g42kx&st=qjvrvbh1&dl=0',
-      'parts_list': 'https://www.dropbox.com/scl/fi/gppfrtuhsdybsfz222xu7/EA-Y1-26-EN-parts-list.pdf?rlkey=3o407jonxacdrc86wj7khxw1h&st=7efo5qic&dl=0',
-      'competition_rules_video': 'https://www.dropbox.com/scl/fi/jtb4vilmbdv7teovma13q/ENJOY-AI-2026-Ancient-Civilizations-Full-Competition-Round.mp4?rlkey=hzr3f43yzcrybhq8kseaium7p&st=05vbykir&dl=0',
-      'full_competition_video': 'https://www.dropbox.com/scl/fi/4ieo2ia6vmlxubl5ydza0/2026-Ancient-civilization-Full-Competition-Round.mp4?rlkey=lyjjv6gtts1mdny826ghyy4t8&st=x2los35r&dl=0',
-      'product_list': 'https://www.dropbox.com/scl/fi/vodwge1rqu635ufkloddo/U20-pro-Part-List.png?rlkey=h1s0hapcsmhxsyz3y229ttnuo&st=xv2kxpba&dl=0',
-      'program_reference': 'https://www.dropbox.com/scl/fo/0riwu037fopipwxu2g69b/AMhRKMnD4YvQKy-NYvahGRo?rlkey=o3jdtjg6ede669s2twmt8i7a6&st=n3o3csff&dl=0',
-      'solution_manual': 'https://www.dropbox.com/scl/fo/ux6ed7i9hxs63hxg7diai/AOfvq1fUQtdlR9gYnKDew-c?rlkey=v8b0xrki0ivmcd6xwis6lk0s9&st=xrsqfkwm&dl=0'
-    }
+  'drone_cup': {
+    'parts_list': 'https://www.dropbox.com/scl/fi/2cokg585k9ayyu41mykun/EA-P10-25-PART-LIST.pdf?rlkey=rl87rg1cn11lbyuhngaicugee&st=uqv13tvs&dl=0',
+    'competition_rule_scoring_sheet': 'https://www.dropbox.com/scl/fi/03gcdquvrs08ou70di1lp/ENJOY-AI-2026-Drone-Cup-Rules-Scoring-Sheet.pdf?rlkey=vu6l2n0gfqk58yls3o7jcrgeg&st=ytvxlbfa&dl=0'
+  },
+  'mining_expedition': {
+    'competition_rule_scoring_sheet': 'https://www.dropbox.com/scl/fi/9f96q35r2nbt95nm2t8pc/2026ver.-Mining-Expedition-Competition-Rule-1.pdf?rlkey=dv3x7qo7s3vfo0lvbh2thdfoq&st=m77t5ahp&dl=0',
+    'field_setup_guide': 'https://www.dropbox.com/scl/fi/mzy44dpqa97chaq6csgio/Mining-Expedition-Competition-Field-Setup-Guide.pdf?rlkey=o7e4djjzfw0vhyiwuvdtjzojh&st=oisb7be2&dl=0',
+    'parts_list': 'https://www.dropbox.com/scl/fi/6pk2fecja7xlmhuo1l3c1/EA-P4-25-Building-Parts-List.pdf?rlkey=ioteom2itpe5axbwejeqtnwm7&st=81y1121l&dl=0',
+    'video_full_competition_round': '', // TODO: 请在此处替换为实际的Dropbox链接
+    'program_reference': '', // TODO: 请在此处替换为实际的Dropbox链接
+    'parts_list2': '' // TODO: 请在此处替换为实际的Dropbox链接
+  },
+  'inventions_trail': {
+    'competition_rule_scoring_sheet': 'https://www.dropbox.com/scl/fi/5qprcf8ezy26gqpey5tnp/compeition-rule.pdf?rlkey=ktvu3885ph0vw8yr6b2d1pr2a&st=qmiin83o&dl=0',
+    'parts_list': 'https://www.dropbox.com/scl/fi/4tb83c3w9zzjfhfxz56ux/EA-P1-26-EN-Parts-List-260212.pdf?rlkey=gcj1tcf0q0lku4tigv2gka13e&st=mmaq57g7&dl=0',
+    'video_competition_rules': 'https://www.dropbox.com/scl/fi/rjhc2q9cddevj2h8dwfqr/ENJOY-AI-2026-Inventions-Trail-Competition-Rules.mp4?rlkey=2sr8ixwpx93g6awtcc4tklv0r&st=b0ws6wk4&dl=0',
+    'video_full_competition_round': 'https://www.dropbox.com/scl/fi/318zktxprqe8dv9cpk6tr/Enjoy-AI-2026-Inventions-Trail-full-competition-round.mp4?rlkey=sw8ccmwoylc28s96te6luvenf&st=n4t77o73&dl=0',
+    'field_setup_guide': 'https://www.dropbox.com/scl/fo/uv1aqaoopgly0tyuyit4i/AFuwkxQHWJ9vW22o75xCdJE?rlkey=9p52ziaha7snk8lmthpwtdh9u&st=kvxtyiic&dl=0',
+    'product_list': 'https://www.dropbox.com/scl/fo/7oj3k7pc0tk4whhw7twjx/AKvxOsp6Is0_lYh0orNvrms?rlkey=8tjej12f7vkka9zjmfhvcy6zo&st=wm1m4bi0&dl=0',
+    'program_reference': 'https://www.dropbox.com/scl/fo/lqj1yp7e15w760sgg4oo1/AJQmPDtNZSocYvMg88rlAhs?rlkey=0009ly68xuzkp5dvcjl6s1ipa&st=1w7bjhor&dl=0',
+    'solution_construction_manual': 'https://www.dropbox.com/scl/fi/k4e3nc9nb738e1wzl054p/Enjoy-AI-2026-Inventions-Trail-Competition-Solution-Demonstration-Manual.pdf?rlkey=ehr2zgwafwwjpxrevqnsgtu8y&st=5tckg3ay&dl=0',
+    'air_pump_user_manual': 'https://www.dropbox.com/scl/fi/r8irw71cbf3bz2z2mdkgg/Air-pump-user-manual.pdf?rlkey=e0koldinv5qozowf7gtca88by&st=96p9s3ze&dl=0'
+  },
+  'battle_of_stars': {
+    'competition_rules': 'https://www.dropbox.com/scl/fo/p8wld9w2b7bzy7znpmz3q/AMxFA6RSBnu6HF8K2kz62eM?rlkey=zmzvdoku6q1uxwpk90mtv44g6&st=o3zp4629&dl=0',
+    'field_setup_guide': 'https://www.dropbox.com/scl/fi/pixz66quabudq32b56krw/ENJOY-AI-2026-Battle-of-stars-field-setup-guide.pdf?rlkey=qc5hnk5objvm0h5b0ibah2qk1&st=qolnnqdp&dl=0',
+    'parts_list': 'https://www.dropbox.com/scl/fi/f8u1nm8d7yus903srm8v2/EA-P2-26-EN-Parts-List.pdf?rlkey=uhrtvyp8vqdmj9vvh5bhm85x6&st=dpjgg35w&dl=0',
+    'video_competition_rules': 'https://www.dropbox.com/scl/fi/mauxwany05050stfngdl0/2026-Battle-of-Stars-Competition-Rules.mp4?rlkey=ljg7amefvy8dgqe19nl66p73x&st=0lmk4erc&dl=0',
+    'video_full_competition_round': 'https://www.dropbox.com/scl/fi/2qr9nquqz1byy2ikjhcw1/2026-Battle-of-Stars-Full-Competition-Round.mp4?rlkey=0ofvjxgjb6rit7vb48nscvj0x&st=0ubxut8k&dl=0',
+    'controller_user_manual': 'https://www.dropbox.com/scl/fi/d9x3wpqd6l65psiv9ohu0/MC102-User-Guide-V1.1_241014.pdf?rlkey=qseqp57zl99iaq4510paaqxes&st=3n1nm6u5&dl=0',
+    'product_list': 'https://www.dropbox.com/scl/fi/y0mabux20g4dxmcsinfwu/AI-Module-3s-Quick-Start-Product-List.pdf?rlkey=2voj08i6skowk4o3nrcz5s0ax&st=5ag84g0c&dl=0',
+    'program_reference': 'https://www.dropbox.com/scl/fo/70u4d6crex7mmkhp7zxms/ALj9a5V5xnF8RN2JkLHXGj4?rlkey=c8m8mi36wlw3jj8tiyz2nb9le&st=mdaj8uvv&dl=0',
+    'solution_construction_manual': 'https://www.dropbox.com/scl/fi/pkuncpsh5jzittha4u1ms/Battle-of-stars-Solution-Construction-Manual.pdf?rlkey=wb8pbqdbzn9ba6f6gtaui3cus&st=p33nx0kf&dl=0'
+  },
+  'skyline_adventure': {
+    'competition_rule': 'https://www.dropbox.com/scl/fo/gl3pgideoqq84z6qw1fjs/AFjsE7V6zI7bYXzPusZbpa4?rlkey=auihwdlhy1z5wulhn4851w6et&st=ph4xrig8&dl=0',
+    'field_setup_guide': 'https://www.dropbox.com/scl/fi/as7b7huhcqicgio4anjwq/ENJOY-AI-2026-Skyline-Adventures-Competition-Field-up-Guide.pdf?rlkey=epdkryoxv4orf2pccdjn83ny9&st=0wgovay3&dl=0',
+    'parts_list': 'https://www.dropbox.com/scl/fo/vhuq1z0hfgi72nl7wmro5/AMsZGJ6QhHCDW-K_Rm6GlL8?rlkey=m8jy5d4gebr7soii8vyti70j6&st=eygt0mpa&dl=0',
+    'video_competition_rules': 'https://www.dropbox.com/scl/fi/c99jphn5fcba0pf5thaed/ENJOY-AI-2026-Skyline-Adventures-Competition-Rules.mp4?rlkey=33sk9ljligp4mosig6ercvumc&st=crqfjm7z&dl=0',
+    'video_full_competition_round': 'https://www.dropbox.com/scl/fi/z3wrh67ikct9j7u872rla/2026-Skyline-Adventure-Full-Competition-Round.mp4?rlkey=sgwghmpnb1xzunycu49140ud4&st=udn2pgvb&dl=0',
+    'controller_user_manual': 'https://www.dropbox.com/scl/fi/0pyomgux7z3ztujpzj5d6/WhalesBot-Eagle_User-Guide_V1.0.pdf?rlkey=rcl7bq0nk9wawm28mt1sgc1k4&st=0laml4cd&dl=0',
+    'product_list': 'https://www.dropbox.com/scl/fi/85yhg5c0msq2ukij9fjkk/Eagle-1003-Quick-Start-Product-List.pdf?rlkey=pa3zyk23phgw8993892m67kvn&st=t76v3q5u&dl=0',
+    'program_reference': 'https://www.dropbox.com/scl/fo/q6eu9ww622umlxxjoj5e3/AJeabNJgkxllD8T2OGSbvYw?rlkey=o7ve809qywc2otl8pw65zd84a&st=uv4uc0kg&dl=0'
+  },
+  'ancient_civilizations': {
+    'competition_rules_field_setup_guide': 'https://www.dropbox.com/scl/fo/abnf2uufaifqhbr0mmplb/AHITzxaVbJ9qhSVuhl1mbwY?rlkey=bt61eqfrwsvofpkzacf2g42kx&st=qjvrvbh1&dl=0',
+    'parts_list': 'https://www.dropbox.com/scl/fi/gppfrtuhsdybsfz222xu7/EA-Y1-26-EN-parts-list.pdf?rlkey=3o407jonxacdrc86wj7khxw1h&st=7efo5qic&dl=0',
+    'video_competition_rules': 'https://www.dropbox.com/scl/fi/jtb4vilmbdv7teovma13q/ENJOY-AI-2026-Ancient-Civilizations-Full-Competition-Round.mp4?rlkey=hzr3f43yzcrybhq8kseaium7p&st=05vbykir&dl=0',
+    'video_full_competition_round': 'https://www.dropbox.com/scl/fi/4ieo2ia6vmlxubl5ydza0/2026-Ancient-civilization-Full-Competition-Round.mp4?rlkey=lyjjv6gtts1mdny826ghyy4t8&st=x2los35r&dl=0',
+    'product_list': 'https://www.dropbox.com/scl/fi/vodwge1rqu635ufkloddo/U20-pro-Part-List.png?rlkey=h1s0hapcsmhxsyz3y229ttnuo&st=xv2kxpba&dl=0',
+    'program_reference': 'https://www.dropbox.com/scl/fo/0riwu037fopipwxu2g69b/AMhRKMnD4YvQKy-NYvahGRo?rlkey=o3jdtjg6ede669s2twmt8i7a6&st=n3o3csff&dl=0',
+    'solution_construction_manual': 'https://www.dropbox.com/scl/fo/ux6ed7i9hxs63hxg7diai/AOfvq1fUQtdlR9gYnKDew-c?rlkey=v8b0xrki0ivmcd6xwis6lk0s9&st=xrsqfkwm&dl=0'
   }
-};
+}
 
 // 链接状态缓存
 let linkStatusCache = {};
@@ -444,25 +454,30 @@ function getDocumentName(year, project, docType) {
     'geometric_forest': 'Geometric Forest',
     'sample_solution': 'Sample Solution',
     'skyline_adventures': 'Skyline Adventures',
-    // 2026年新增项目映射
+    // 2026年项目映射
+    'drone_cup': 'Drone Cup',
+    'mining_expedition': 'Mining Expedition',
     'inventions_trail': 'Inventions Trail',
     'battle_of_stars': 'Battle of Stars',
+    'skyline_adventure': 'Skyline Adventure',
     'ancient_civilizations': 'Ancient Civilizations'
-    // skyline_adventures 已存在，不需要重复添加
   };
   
   const docTypeNames = {
     'competition_rule': 'Competition Rule',
     'field_setup_guide': 'Field Setup Guide',
     'parts_list': 'Parts List',
-    'competition_rules_video': 'Competition Rules Video',
-    'full_competition_video': 'Full Competition Video',
-    'product_list': 'Product List',
+    'competition_rule_scoring_sheet': 'Competition Rule & Scoring Sheet',
+    'video_full_competition_round': 'Video: Full Competition Round',
     'program_reference': 'Program Reference',
-    'solution_manual': 'Solution Construction Manual',
-    'air_pump_manual': 'Air Pump User Manual',
-    'controller_manual': 'Controller User Manual',
-    'competition_rules': 'Competition Rules'
+    'video_competition_rules': 'Video: Competition Rules',
+    'product_list': 'Product List',
+    'solution_construction_manual': 'Solution Construction Manual',
+    'air_pump_user_manual': 'Air Pump User Manual',
+    'competition_rules': 'Competition Rules',
+    'controller_user_manual': 'Controller User Manual',
+    'competition_rules_field_setup_guide': 'Competition Rules & Field Setup Guide',
+    'parts_list2': 'Parts List 2'
   };
   
   return `${year} - ${projectNames[project] || project} - ${docTypeNames[docType] || docType}`;
